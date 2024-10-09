bcc8cb768b7466d4cd5cd3dfa7d64404a599181d
RickyPWebDev <100093181+RickyPWebDev@users.noreply.github.com>
2024-10-09 16:28:39 +0100

    Add files via upload

---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function NavBar() {
+  return (
+    <div className="top-bar">
+      <h1>Welcome to the spare income calculator</h1>
+    
+    </div>
+  );
+}
+
+export default NavBar;
---
+++ src/Output.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+function Output() {
+    return (
+      <div className="App">
+        
+        
+   
+      </div>
+    );
+  }
+  
+  export default Output;
